4a0711a178b1e33528e0b566358a03dd23d9570c
souleima <zghab2010@hotmail.fr>
2017-04-17 22:30:45 +0100

    Test2

---
+++ PPP_Salaire/Repositories/CongeRepository.cs
@@ -0,0 +1,31 @@
+﻿using PPP_Salaire.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PPP_Salaire.Repositories
+{
+    public class CongeRepository
+    {
+        PPPDBContext db = new PPPDBContext();
+
+        public IList<Conge> Lister()
+        {
+            return this.db.Conges.ToList();
+        }
+
+        public Conge GetById(int id)
+        {
+            return this.db.Conges.Find(id);
+        }
+
+        public void Update(Conge c)
+        {
+            this.db.Entry(c).State = EntityState.Modified;
+            this.db.SaveChanges();
+        }
+
+    }
+}
